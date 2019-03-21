fc9b934fdd1dbe8323ec59fc6ef55c541b2a3c71
jacniluk <jacniluk@wp.pl>
2019-03-22 00:22:05 +0100

    Initial
    Initial commit with project basics.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Offset camera to player
+    private Vector3 offset;
+
+    // Player the camera belongs to
+    public GameObject player;
+    // Smooth speed
+    public float smooth = 6.0f;
+
+    // Called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Called after all Update functions have been called
+    void LateUpdate()
+    {
+        // Move camera with smooth
+        Vector3 destination = player.transform.position + offset;
+        transform.position = Vector3.Lerp(transform.position, destination, smooth * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Components
+    // Rigidbody
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Called every fixed frame-rate frame
+    void FixedUpdate()
+    {
+        // Update position
+        Vector3 move = new Vector3(0.0f, 0.0f, 0.0f);
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            move.z = 1.0f;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        {
+            move.z = -1.0f;
+        }
+        else if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            move.x = -1.0f;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            move.x = 1.0f;
+        }
+        rb.MovePosition(transform.position + move);
+    }
+}
